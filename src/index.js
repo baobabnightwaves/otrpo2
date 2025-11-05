959bb299acfd7118f6c4f81da86292f723a8102c
baobabnightwaves@outlook.com <baobabnightwaves@outlook.com>
2025-11-06 02:24:27 +0500

    ываы

--- src/index.js
+++ src/index.js
@@ -74,3 +74,10 @@ document.querySelector('.btn-light').addEventListener('click', () => {
     const toast = new bootstrap.Toast(toastEl);
     toast.show();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+});
